48e0999f4c3478a5d5f9f479313db1188945e270
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-05-16 06:38:10 +0100

    btc address changed

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'bitcoin_qr' => 'images/btc_qr.svg',
-    'bitcoin_wallet_address' => 'dfondfoindfoiidfoidfdofdf',
-    'ethereum_qr' => 'images/btc_qr.svg',
-    'ethereum_wallet_address' => '0i9jdjfidmfpoedfidfidfi0oijdfo',
+    'bitcoin_qr' => 'images/btc.png',
+    'bitcoin_wallet_address' => env('BTC_ADDRESS','1J5CM6ZFu1yy8T68USXPGEWjUpd3h1XJ2m'),
+    'ethereum_qr' => 'images/eth.png',
+    'ethereum_wallet_address' => env('ETH_ADDRESS','0x6C5924ff5A53304bDcA5a3c703a1BE53991F19A4'),
     'deposit_image'=>'assets/deposits',
     'address' => env('ADDRESS','some random address'),
     'admin_notification_email' => env('ADMIN_NOTIFICATION_EMAIL','perepaul15@gmail.com'),
