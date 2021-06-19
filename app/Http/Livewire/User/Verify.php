9ade80b6654a89898eafea5a9413a1bbc0b5590c
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-06-19 16:34:25 +0100

    removing forceful number zips

--- app/Http/Livewire/User/Verify.php
+++ app/Http/Livewire/User/Verify.php
@@ -36,7 +36,7 @@ class Verify extends Component
             'data.city' =>'required',
             'data.currency_id'=>'required|numeric|integer',
             'data.address' => 'required',
-            'data.zip_code' => 'bail|required|numeric|min:5',
+            'data.zip_code' => 'bail|required|string|min:5',
             'data.id_file' => 'required|file|mimes:jpeg,jpg,png|max:1024'
         ];
     }
