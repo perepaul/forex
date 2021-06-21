137c72e2dcc42352264a60e5e46d057522d62e97
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-06-21 20:58:12 +0100

    formatted live chat

--- resources/views/components/live-chat.blade.php
+++ resources/views/components/live-chat.blade.php
@@ -1,16 +1,14 @@
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function() {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/60d0e9ec65b7290ac63723d0/1f8o13hnl';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
+<!--StartofTawk.toScript-->
+<script type="text /javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),
+        s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/60d0ee8a7f4b000ac038c74d/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
     })();
-
 </script>
 <!--End of Tawk.to Script-->
