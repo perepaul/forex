0da5170814288efa8de65fa575762f55679eef12
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-07-09 12:32:18 +0100

    Live chat modification

--- resources/views/components/live-chat.blade.php
+++ resources/views/components/live-chat.blade.php
@@ -1,14 +1,3 @@
 <!--StartofTawk.toScript-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),
-        s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/60d0ee8a7f4b000ac038c74d/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
+{!! config('constants.live_chat') !!}
 <!--End of Tawk.to Script-->
