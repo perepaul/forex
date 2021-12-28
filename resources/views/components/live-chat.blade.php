827ef179d6a147d4d964854cc553fe95e92000f6
emma-kodes <perepaulonline@gmail.com>
2021-12-28 11:06:13 +0000

    whatsapp button

--- resources/views/components/live-chat.blade.php
+++ resources/views/components/live-chat.blade.php
@@ -9,5 +9,21 @@
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
     })();
-    </script>
-    <!--End of Tawk.to Script-->
+</script>
+
+<!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+            var options = {
+                whatsapp: "+1 (435) 233‑7159", // WhatsApp number
+                call_to_action: "Message us", // Call to action
+                position: "left", // Position may be 'right' or 'left'
+            };
+            var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+</script>
+<!-- /GetButton.io widget -->
+<!--End of Tawk.to Script-->
